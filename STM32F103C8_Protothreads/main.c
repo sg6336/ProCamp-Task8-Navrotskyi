9b810caceb2211b811222cced09d532df5394b0e
Denys <navrotskyidenys@gmail.com>
2021-09-24 23:39:36 +0300

    PT_SCHEDULE and PT_WAIT_UNTIL

--- STM32F103C8_Protothreads/main.c
+++ STM32F103C8_Protothreads/main.c
@@ -1,22 +1,54 @@
+#include "pt-1.4/pt.h"
 
-#include ".\pt-1.4\pt.h"
+#include <stdio.h>
+
+//struct pt { unsigned short lc; };
+//#define PT_THREAD(name_args)  char name_args
+//#define PT_BEGIN(pt)          switch(pt->lc) { case 0:
+//#define PT_WAIT_UNTIL(pt, c)  pt->lc = __LINE__; case __LINE__: \
+//                              if(!(c)) return 0
+//#define PT_END(pt)            } pt->lc = 0; return 2
+//#define PT_INIT(pt)           pt->lc = 0
 
 static int counter;
-static struct pt example_pt;
+static struct pt pt3;
 
-struct pt pt;
-
-static
-PT_THREAD(example(struct pt* pt))
+static PT_THREAD(protothread1(struct pt* pt))
 {
     PT_BEGIN(pt);
 
     while (1) {
-        //pt->lc = 15; case 15: if (!(counter == 1000)) return 0;
-        PT_WAIT_UNTIL(pt, counter == 1000);
-        printf("Threshold reached. Counter = %d\n", counter);
+        PT_WAIT_UNTIL(pt, counter == 10);
+        printf("protothread1. Counter = %d\n", counter);
+        counter = 15;
+    }
+
+    PT_END(pt);
+}
+
+static PT_THREAD(protothread2(struct pt* pt))
+{
+    PT_BEGIN(pt);
+
+    while (1) {
+        PT_WAIT_UNTIL(pt, counter == 20);
+        printf("protothread2. Counter = %d\n", counter);
         counter = 0;
     }
+
+    PT_END(pt);
+}
+
+static PT_THREAD(protothread3(struct pt* pt))
+{
+    static struct pt pt1, pt2;
+
+    PT_BEGIN(pt);
+
+    PT_INIT(&pt1);
+    PT_INIT(&pt2);
+
+    PT_WAIT_THREAD(pt, protothread1(&pt1) & protothread2(&pt2));
 
     PT_END(pt);
 }
@@ -24,10 +56,8 @@ PT_THREAD(example(struct pt* pt))
 int main(void)
 {
     counter = 0;
-    PT_INIT(&example_pt);
 
-    while (1) {
-        example(&example_pt);
+    while (PT_SCHEDULE(protothread3(&pt3))) {
         counter++;
     }
     return 0;
